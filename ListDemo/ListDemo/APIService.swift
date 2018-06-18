1fd51523fb9db0817f4d8cb2970c1520a4cfbc4f
Jitendra.deore <jitendra.deore@verse.in>
2018-06-18 17:23:43 +0530

    API Service class. Added class method to get the data from server and return the appropriate model. Using URLSession to make the request.

--- ListDemo/ListDemo/APIService.swift
+++ ListDemo/ListDemo/APIService.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+enum Result<T>{
+    case success(T)
+    case failure(Error?)
+}
+
+class APIService {
+    
+    class func fetchData(apiURL: String,onCompletion:@escaping (Result<List>)-> Void) {
+        
+        // Create the URLSession on the default configuration
+        let defaultSessionConfiguration = URLSessionConfiguration.default
+        let defaultSession = URLSession(configuration: defaultSessionConfiguration)
+        
+        // Passing the search controller text ...
+        // Setup the request with URL
+        
+        let url = URL(string: apiURL)
+        var urlRequest = URLRequest(url: url!)  // Note: This is a demo, that's why I use implicitly unwrapped optional
+        
+        // Set the httpMethod and assign httpBody
+        urlRequest.httpMethod = "GET"
+        
+        // Create dataTask
+        let dataTask = defaultSession.dataTask(with: urlRequest) {(data, response, error) in
+            if error != nil {
+                print(error ?? "")
+                onCompletion(.failure(error))
+            } else {
+                do {
+                    let asciJSON = String(data: data!, encoding: String.Encoding.ascii)
+                    if let response = try JSONSerialization.jsonObject(with:(asciJSON?.data(using: String.Encoding.utf8))!, options: []) as? JSONItem{
+                        if  let listItem = List(json: response){
+                            DispatchQueue.main.async {
+                                onCompletion(.success(listItem))
+                            }
+                        }
+                    }
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+        }
+        // Fire the request
+        dataTask.resume()
+    }
+    
+}
+
