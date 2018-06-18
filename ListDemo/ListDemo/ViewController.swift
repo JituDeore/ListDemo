3a9bea53122a1533a0e786ffb2face5fb6949f19
Jitendra.deore <jitendra.deore@verse.in>
2018-06-18 17:31:27 +0530

    Making an API call to get the list details. Using API Service class to fetch the data. Passing the URL as a parameter and added completion handler to notify the view controller task has been completed with the response + error state if any.

--- ListDemo/ListDemo/ViewController.swift
+++ ListDemo/ListDemo/ViewController.swift
@@ -20,7 +20,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        APIService.fetchData(apiURL: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json") {  [weak self] (result) in
+            guard let strongSelf = self else {
+                return
+            }
+            switch result{
+            case .success(let response):
+                strongSelf.listItems = response.listItems
+                strongSelf.navTitle = response.title
+                self?.title = response.title
+                
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
