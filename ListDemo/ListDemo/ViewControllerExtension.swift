6bc5f9d0839b0f3ee2df320ad81c61674fe36956
Jitendra.deore <jitendra.deore@verse.in>
2018-06-20 18:49:36 +0530

    Added ListViewController extension to operate out the UIActivityIndicatorView functions.

---
+++ ListDemo/ListDemo/ViewControllerExtension.swift
@@ -0,0 +1,42 @@
+//
+//  ViewControllerExtension.swift
+//  ListDemo
+//
+//  Created by Jitendra Deore on 20/06/18.
+//  Copyright © 2018 Jitendra Deore. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ListViewController{
+   
+    /**
+     Shows progress.
+     */
+    func showProgress() {
+        if progressView == nil {
+            let progressView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+            view.addSubview(progressView)
+            
+            progressView.translatesAutoresizingMaskIntoConstraints = false;
+            view.addConstraint(NSLayoutConstraint(item: progressView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0))
+            view.addConstraint(NSLayoutConstraint(item: progressView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0))
+            progressView.startAnimating()
+            self.progressView = progressView
+        }else{
+            self.progressView?.startAnimating()
+        }
+    }
+    
+    /**
+     Hides progress.
+     */
+    func hideProgress() {
+        progressView?.stopAnimating()
+        _ = progressView?.hidesWhenStopped
+    }
+    
+    
+}
+
