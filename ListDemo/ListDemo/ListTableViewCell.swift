07bd67d1dae4516244acdc78a50ba7a8a308d7c1
Jitendra.deore <jitendra.deore@verse.in>
2018-06-20 17:49:19 +0530

    Added UITableViewCell class - This class is responsible for to show the list data. Added ListViewController extension - Here we have added UIActivityIndicatorView logic this we have added in extension to make the code modular.

---
+++ ListDemo/ListDemo/ListTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ListTableViewCell.swift
+//  ListDemo
+//
+//  Created by Jitendra Deore on 20/06/18.
+//  Copyright © 2018 Jitendra Deore. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func displayData(with listItem: ListItem) {
+        self.titleLabel.text = listItem.title
+        self.descriptionLabel.text = listItem.description
+        if let imageURL = listItem.imageURL{
+            thumbnailImageView?.sd_setImage(with: URL(string: imageURL), placeholderImage: #imageLiteral(resourceName: "placeHolderImage"))
+        }
+    }
+}
