b23d5bb5e5e30db430b18490022ddf851a3eda5d
Jitendra.deore <jitendra.deore@verse.in>
2018-06-21 22:41:37 +0530

    Created protocol to handle the error view. Added all error condition over UIViewController extension so anyone can use this across the app anywhere.

--- ListDemo/ListDemo/ErrorPresentableViewController.swift
+++ ListDemo/ListDemo/ErrorPresentableViewController.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2018 Jitendra Deore. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+/**
+ ErrorPresentableViewController needs to be implemented by UIViewControllers
+ which wants to display error full page, in a view which encapsulates
+ ListViewController.
+ */
+protocol ErrorPresentableViewController {
+    /// ErrorView which will be used to render error.
+    var errorView: ErrorView    { get }
+    
+    var errorContainer: UIView { get }
+    
+    /// Asks the view controller whether fullscreen error can be presented
+    var canFullScreenErrorBePresented: Bool      { get }
+}
+
+extension ErrorPresentableViewController where Self: UIViewController {
+    
+    /// Adds a full page error to the caller view
+    func handlePageError(_ _error: Error?, isHeadline: Bool = false) {
+        guard let error = _error as NSError? else {
+            return
+        }
+        switch error.code {
+        case ListErrorCodes.noNetWork:
+            showErrorScreenIfRequired(errorContentType: .noNetwork)
+        case ListErrorCodes.noContent:
+            showErrorScreenIfRequired(errorContentType: .noContent)
+        case ListErrorCodes.timeOut:
+            showErrorScreenIfRequired(errorContentType: .serverTimeout)
+        default:
+            showErrorScreenIfRequired(errorContentType: .defaultError)
+            //contentListViewController.showInfoMessage(message: "Something Went Wrong", withLoder: false)
+            break
+        }
+    }
+    func removeErrorScreen() {
+        self.errorView.removeErrorView()
+    }
+    
+    private func showErrorScreenIfRequired(errorContentType: ErrorContentType) {
+        if  canFullScreenErrorBePresented  {
+            errorView.presentInView(parentView: errorContainer, errorContent: errorContentType)
+        }
+    }
+    
+    var errorContainer: UIView { return view }
+}
+
