608e56500c6e633c91282bab515afaf79ac4673c
Jitendra.deore <jitendra.deore@verse.in>
2018-06-21 15:41:06 +0530

    Added ErrorPresentableViewController to show the error.

---
+++ ListDemo/ListDemo/ErrorPresentableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorPresentableViewController.swift
+//  ListDemo
+//
+//  Created by Jitendra Deore on 21/06/18.
+//  Copyright © 2018 Jitendra Deore. All rights reserved.
+//
+
+import Foundation
